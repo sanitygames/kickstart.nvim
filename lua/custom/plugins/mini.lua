59d09ed23d71b5d4a75cec48eee88eba935249c8
sanitygames0436 <sanitygames04@gmail.com>
2025-01-03 17:13:30 +0900

    disabled mini.sessions

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -16,21 +16,16 @@ return { -- Collection of various small independent plugins/modules
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
+      require('mini.jump').setup()
 
       -- MiniSessions
       -- TODO: バッファのゴミみたいなのが出る。
-      require('mini.sessions').setup {
-        autowrite = false,
-        directory = '~/.config/nvim/sessions',
-        file = '',
-      }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      -- require('mini.surround').setup()
+      -- require('mini.sessions').setup {
+      --   autowrite = false,
+      --   directory = '~/.config/nvim/sessions',
+      --   file = '',
+      -- }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
@@ -49,7 +44,6 @@ return { -- Collection of various small independent plugins/modules
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
-      require('mini.jump').setup()
 
       require('mini.starter').setup {
         header = [[
