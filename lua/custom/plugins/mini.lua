94c68bbf3a177ed6da0d0003697a77ab6b4e8edf
sanitygames0436 <sanitygames04@gmail.com>
2025-01-03 15:41:20 +0900

    stable mini.lua

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,41 +1,52 @@
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
-  config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup { n_lines = 500 }
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    -- require('mini.surround').setup()
+  -- mini.nvimの設定
+  {
+    'echasnovski/mini.nvim',
+    lazy = false,
+    keys = {
+      { '<leader>S', '<cmd>lua MiniSessions.select()<CR>', desc = 'MiniSessions.select' }
+    },
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
 
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
+      require('mini.sessions').setup {
+        directory = '~/.config/nvim/sessions',
+        file = '',
+      }
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      -- require('mini.surround').setup()
 
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
 
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
-    require('mini.jump').setup()
-    require('mini.starter').setup {
-      header = [[
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.jump').setup()
+      require('mini.starter').setup {
+        header = [[
   .-')       ('-.          .-') _             .-') _
  ( OO ).    ( OO ).-.     ( OO ) )           (  OO) )
 (_)---\_)   / . --. / ,--./ ,--,'    ,-.-')  /     '._    ,--.   ,--.
@@ -55,17 +66,9 @@ return { -- Collection of various small independent plugins/modules
  |  '--'  |    |  | |  |  |  |   |  |  |  `---. \       /
   `------'     `--' `--'  `--'   `--'  `------'  `-----'
         ]],
-    }
 
-    vim.keymap.set('n', '<leader>S', '<cmd>lua MiniSessions.select()<CR>', { desc = 'test' })
-    require('mini.sessions').setup {
-      directory = '~/.config/nvim/sessions',
-      file = '',
-      -- keys = {
-      --   { '<leader>S', '<cmd>lua MiniSessions.select()<CR>' }
-      -- }
-      -- vim.keymap.set('n', '<leader>S', [[<cmd>lua MiniSessions.select()<CR>]])
 
-    }
-  end,
+      }
+    end,
+  }
 }
