40f1beff35a77279d39ed20674d8e7aee4b5f404
sanitygames0436 <sanitygames04@gmail.com>
2025-01-07 10:30:53 +0900

    disable incline.nvim

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -13,35 +13,35 @@ return {
 
   -- incline.nvim の設定
   -- 右上のファイル名表示
-  {
-    'b0o/incline.nvim',
-    event = 'VeryLazy',
-    config = function()
-      local helpers = require 'incline.helpers'
-      local devicons = require 'nvim-web-devicons'
-      require('incline').setup {
-        window = {
-          padding = 0,
-          margin = { horizontal = 0 },
-        },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-          if filename == '' then
-            filename = '[no name]'
-          end
-          local ft_icon, ft_color = devicons.get_icon_color(filename)
-          local modified = vim.bo[props.buf].modified
-          return {
-            ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
-            ' ',
-            { filename, gui = modified and 'bold,italic' or 'bold' },
-            ' ',
-            guibg = '#44406e',
-          }
-        end,
-      }
-    end,
-  },
+  -- {
+  --   'b0o/incline.nvim',
+  --   event = 'VeryLazy',
+  --   config = function()
+  --     local helpers = require 'incline.helpers'
+  --     local devicons = require 'nvim-web-devicons'
+  --     require('incline').setup {
+  --       window = {
+  --         padding = 0,
+  --         margin = { horizontal = 0 },
+  --       },
+  --       render = function(props)
+  --         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+  --         if filename == '' then
+  --           filename = '[no name]'
+  --         end
+  --         local ft_icon, ft_color = devicons.get_icon_color(filename)
+  --         local modified = vim.bo[props.buf].modified
+  --         return {
+  --           ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+  --           ' ',
+  --           { filename, gui = modified and 'bold,italic' or 'bold' },
+  --           ' ',
+  --           guibg = '#44406e',
+  --         }
+  --       end,
+  --     }
+  --   end,
+  -- },
 
   -- barbar.nvim の設定
   -- タブ移動を便利にする。
