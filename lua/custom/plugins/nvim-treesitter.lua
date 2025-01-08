4b8f5d654748a5aa7e4abcb06b8f1b40e1c4d86a
sanitygames0436 <sanitygames04@gmail.com>
2025-01-08 19:14:27 +0900

    move conform, nvim-treesitter

---
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -0,0 +1,56 @@
+return {
+    { -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+        opts = {
+            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'gdscript', 'supercollider' },
+            -- Autoinstall languages that are not installed
+            auto_install = true,
+            highlight = {
+                enable = true,
+                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+                --  If you are experiencing weird indenting issues, add the language to
+                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+                additional_vim_regex_highlighting = { 'ruby' },
+            },
+            indent = { enable = true, disable = { 'ruby' } },
+        },
+
+        -- NOTE: supercollider settings
+        config = function()
+            local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+            parser_config.supercollider = {
+                install_info = {
+                    url = 'https://github.com/madskjeldgaard/tree-sitter-supercollider',
+                    files = { 'src/parser.c', 'src/scanner.c' },
+                    maintainer = '@madskjeldgaard',
+                },
+                filetype = 'supercollider',
+            }
+            require('nvim-treesitter.configs').setup {
+                textsubjects = {
+                    enable = true,
+                    prev_selection = ',',
+                    keymaps = {
+                        ['.'] = 'textsubjects-smart',
+                        [';'] = 'textsubjects-container-outer',
+                        ['i;'] = { 'textsubjects-container-inner', desc = "Select inside containers (classed, functions, etc.)" },
+                    },
+                },
+            }
+        end,
+        --
+        -- There are additional nvim-treesitter modules that you can use to interact
+        -- with nvim-treesitter. You should go explore a few and see what interests you:
+        --
+        --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+        --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+        --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    },
+
+    {
+        'RRethy/nvim-treesitter-textsubjects'
+    },
+}
