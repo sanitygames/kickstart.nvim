7f8ace8c312deadc6f18b7c8758ed370b22c63a5
sanitygames0436 <sanitygames04@gmail.com>
2025-01-08 19:15:45 +0900

    move autocmds

--- init.lua
+++ init.lua
@@ -15,20 +15,10 @@
 
 require('options')
 require('keymaps')
+require('autocmds')
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-})
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -52,14 +42,12 @@ require('lazy').setup({
     -- Use `opts = {}` to force a plugin to be loaded.
     --
 
-
     -- NOTE: Plugins can specify dependencies.
     --
     -- The dependencies are proper plugin specifications as well - anything
     -- you do for a plugin at the top level, you can do for a dependency.
     --
     -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
 
     -- LSP Plugins
     {
